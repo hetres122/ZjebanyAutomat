2396a8dc0ee2fca143c338dc2fa6098d312fd5c5
hetres122 <61289464+hetres122@users.noreply.github.com>
2020-03-05 21:33:58 +0100

    Update

--- Script/Script.js
+++ Script/Script.js
@@ -54,18 +54,56 @@ const automat = {
 
     produkty() {
         var newProduct = "";
-        var newProductPrice = "";
-
+        var newProductPrice = 0;
+        var newQuantity = 0;
+        var newID = 0;
+        var ID = 0;
+        var quantity = 0;
+        var productPrice = 0;
+        var product = "";
         const add = document.querySelector('.add');
         add.addEventListener('click', (event) => {
             newProduct = document.querySelector("#product");
             newProductPrice = document.querySelector("#price");
+            newQuantity = document.querySelector("#quantity");
+            newID = document.querySelector("#id");
+            ID = newID.value;
+            quantity = newQuantity.value;
+            productPrice = newProductPrice.value;
+            product = newProduct.value;
 
-            console.log(newProduct.value);
-            console.log(newProductPrice.value);
+            console.log(this.products);
+            this.products.product = {};
+            // this.products.product.id = +ID;
+            // this.products.product.value = +productPrice;
+            // this.products.product.quantity = +quantity;
+            console.log(this.products);
+        });
+    },
+    dodwanie() {
+
+        const baton1 = document.querySelector('.b1');
+        const baton2 = document.querySelector('.b2');
+        const baton3 = document.querySelector('.b3');
+        const baton4 = document.querySelector('.b4');
+
+        baton1.addEventListener('click', (event) => {
+
+        });
+
+        baton2.addEventListener('click', (event) => {
+
+        });
+
+        baton3.addEventListener('click', (event) => {
+
+        });
+
+        baton4.addEventListener('click', (event) => {
 
         });
     },
 }
 automat.produkty();
 automat.controlPanel();
+automat.dodwanie();
