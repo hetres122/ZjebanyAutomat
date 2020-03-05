0543e7a9f37b3d9a50450c3401d15283240c711c
hetres122 <61289464+hetres122@users.noreply.github.com>
2020-03-05 19:03:37 +0100

    Update

--- Script/Script.js
+++ Script/Script.js
@@ -1,18 +1,71 @@
 const automat = {
-    nazwa: "",
-    cena: 0,
+    loginAdmin: "root",
+    passwordAdmin: "root",
+    products: {
+        blackCoffee: {
+            id: 0,
+            value: 2.5,
+            quantity: 100
+        },
+    },
+    controlPanel() {
+        var move = document.querySelector('.pA');
+        var automatClick = document.querySelector('.Automat1')
+        var page1 = document.querySelector('.page1');
+        var page2 = document.querySelector('.page2');
+        var page3 = document.querySelector('.page3');
+        var login = "";
+        var password = "";
+        var automatClick2 = document.querySelector('.Automat2');
 
-    produkty() {
-        const newProduct = document.forms[0];
-        const newProductPrice = document.querySelector("#price");
-        const btn = document.querySelector('.btn');
-        btn.addEventListener('click', (event) => {
 
-            console.log(newProduct);
-            console.log(newProductPrice);
+        move.addEventListener('click', (event) => {
+            page1.style.display = "none";
+            page2.style.display = "block";
         });
+        automatClick.addEventListener('click', (event) => {
+            page2.style.display = "none"
+            page1.style.display = "block"
+
+        });
+
+        var signIn = document.querySelector('.signIn');
+        signIn.addEventListener('click', (event) => {
+            login = document.querySelector('#login');
+            password = document.querySelector('#password');
+            if (login.value == this.loginAdmin && password.value == this.passwordAdmin) {
+
+                page2.style.display = "none";
+                page3.style.display = "block";
+
+            } else {
+                alert("Wrong password");
+
+            }
+
+        });
+        automatClick2.addEventListener('click', (event) => {
+            page3.style.display = "none";
+            page1.style.display = "block";
+        });
+
     },
 
 
+    produkty() {
+        var newProduct = "";
+        var newProductPrice = "";
+
+        const add = document.querySelector('.add');
+        add.addEventListener('click', (event) => {
+            newProduct = document.querySelector("#product");
+            newProductPrice = document.querySelector("#price");
+
+            console.log(newProduct.value);
+            console.log(newProductPrice.value);
+
+        });
+    },
 }
 automat.produkty();
+automat.controlPanel();
